b5d689d2efabc62feb6bf06f62b5d8bc4a223e0a
Guo Mingxuan <carlguo26@gmail.com>
2017-11-04 23:22:02 +0800

    initial commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>CodeIgniter Tutorial</title>
+	</head>
+	<body>
+		<h1>
+			<h5>this is a fucking about</h5>
+			<?php echo $title; ?>
+		</h1>
+	</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>CodeIgniter Tutorial</title>
+	</head>
+	<body>
+		<h1>
+			<?php echo $title; ?>
+			<em>&copy; 2015</em>
+		</h1>
+	</body>
+</html>
